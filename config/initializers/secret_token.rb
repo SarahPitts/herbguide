f99b329974adda06e05007d44f28561d3b05186d
Sarah Pitts <mccallsmedia@gmail.com>
2013-05-22 13:41:03 -0500

    basic herb guide first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herbguide::Application.config.secret_token = '9d1d0e4204f5086125e16ba2edd68ed8529d2f7a609221145da5eb986bc7f02ec7c0b674c8e1497c3bd0d4d71531243c79df3555c779813b05035b9a8d475df9'
